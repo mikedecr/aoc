ef8afd22bdc2915c00a0089c61d7e07eda0324c7
mikedecr <mgdecrescenzo@gmail.com>
2024-01-09 22:36:50 -0600

    redo 4.1, finish 4.2

--- jl/04.jl
+++ jl/04.jl
@@ -1,24 +1,74 @@
+using Base.Iterators: peel
+using Chain
 
-not(f) = (a...) -> f(a...) == false
-drop_empties(l) = filter(not(isempty), l)
-as_integer(s) = parse(Int, s)
+#===================#
+#=    card type    =#
+#===================#
 
-function score_card(line)
-    left, right = split(line, "|")
-    haystack = split(split(left, ":")[2], " ") |> drop_empties
-    needles = split(right, " ") |> drop_empties
-    n_matches = filter(n -> n in haystack, needles) |> length
-    n_matches == 0 && return 0
-    return 2^(n_matches - 1)
+struct Card
+    winners::Vector{String}
+    held::Vector{String}
 end
 
-function part1(inputs)
-    score_card.(inputs) |> sum
+function parse_card(text::String)
+    wins, held = @chain text begin
+        split(": ")
+        getindex(2)
+        split("|")
+        split.()
+    end
+    Card(wins, held)
 end
 
+#===============#
+#=    first    =#
+#===============#
 
-@assert part1(readlines("data/04/test.txt")) == 13
+function n_matches(card::Card)::Int
+    filter(x -> x in card.winners, card.held) |> length
+end
 
-part1(readlines("data/04/final.txt"))
+function score_matches(n::Int)::Int
+    n == 0 && return 0
+    return 2^(n - 1)
+end
 
+function part_one(file::String)
+    score_line = l -> l |> parse_card |> n_matches |> score_matches
+    file |> eachline .|> score_line |> sum
+end
+
+@assert part_one("data/04/test.txt") == 13
+println(part_one("data/04/final.txt"))
+
+
+#================#
+#=    second    =#
+#================#
+
+test = "data/04/test.txt"
+final =  "data/04/final.txt"
+test |> readlines |> first |> parse_card |> n_matches
+
+# this is kinda hacky but whatever
+function increment_coefs(coefs, i, n)
+    i == 0 && return(coefs)
+    r = range(1, n) .+ i
+    multiple = coefs[i]
+    coefs[r] = coefs[r] .+ multiple
+    return coefs
+end
+
+function part_two(filename::String)
+    cards = readlines(filename) .|> parse_card
+    coefs = repeat([1], length(cards))
+    for (i, c) in enumerate(cards)
+        n = n_matches(c)
+        coefs = increment_coefs(coefs, i, n)
+    end
+    return sum(coefs)
+end
+
+@assert part_two(test) == 30
+println(part_two(final))
 
